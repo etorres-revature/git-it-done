69be9e01f0d46b4ae65c4e5369e30b4a84a6f073
Eric D. Torres <etorresnotary@gmail.com>
2021-09-22 23:40:33 -0500

    display repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,17 +1,17 @@
 const userFormEl = document.querySelector("#user-form");
 const nameInputEl = document.querySelector("#username");
 
-const getUserRepos = function(user) {
-    // format the github api url
-    let apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
+const getUserRepos = function (user) {
+  // format the github api url
+  let apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      displayRepose(data, user);
     });
-  };
+  });
+};
 
 const formSubmitHandler = (event) => {
   event.preventDefault();
@@ -24,6 +24,11 @@ const formSubmitHandler = (event) => {
   } else {
     alert("Please enter a GitHub username.");
   }
+};
+
+const displayRepose = function (repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
