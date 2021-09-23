7cdb1167fa0e21f19b2f0ded44c61faa2afb8e1e
Eric D. Torres <etorresnotary@gmail.com>
2021-09-23 00:26:06 -0500

    function to display repos

--- assets/js/single-repo.js
+++ assets/js/single-repo.js
@@ -11,8 +11,38 @@ const getRepoIssues = function (repo) {
       }
     })
     .then(function (data) {
-      console.log(data);
+      displayIssues(data);
     });
+};
+
+const displayIssues = function (issues) {
+  for (let i = 0; i < issues.length; i++) {
+    // create link element to take users to the issue on GH
+    let issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+
+    // create span to hold issue title
+    let titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].titleEl;
+
+    // append to container
+    issueEl.appendChild(titleEl);
+
+    // create a type element
+    let typeEl = document.createElement("span");
+
+    // check if issue is an actual issue of pull request
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull Request)";
+    } else {
+      typeEl.textContent = "(Issue)";
+    }
+
+    // append to container
+    issueEl.appendChild(typeEl);
+  }
 };
 
 getRepoIssues("facebook/react");
