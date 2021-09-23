6d2a8ec41f471abfeadf6b00579e947b2c892b3b
Eric D. Torres <etorresnotary@gmail.com>
2021-09-22 23:29:08 -0500

    FE form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,14 +2,12 @@ const getUserRepos = (user) => {
   let apiURL = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
-  fetch(apiUrl)
-  .then((response => {
+  fetch(apiURL)
+  .then((response) => {
       response.json();
   })
   .then((data) => {
       console.log(data)
-  } {
-
   })
 };
 
