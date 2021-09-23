5580d8e7067a4efb9547a2198494d2db3f40e6fd
Eric D. Torres <etorresnotary@gmail.com>
2021-09-22 23:55:42 -0500

    issues icon

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -49,6 +49,24 @@ const displayRepose = function (repos, searchTerm) {
     //append to container
     repoEl.appendChild(titleEl);
 
+    // create a status element
+    let statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-icon icon-danger'</i>" +
+        repos[i].open_issues_count +
+        " issue(s)...";
+    } else {
+      statusEl.innerHTML =
+        "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    // append to container
+    repoEl.appendChild(statusEl);
+
     // append to container to the DOM
     repoContainerEl.appendChild(repoEl);
   }
